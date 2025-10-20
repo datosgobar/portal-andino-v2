f13eb9c68f395ea53d0b2a976b650d6da0d27561
PavloAE <pavloae@gmail.com>
2025-10-20 17:38:15 -0300

    Reemplaza datapusher por Xloader

---
+++ ckan/docker-entrypoint.d/02_setup_xloader.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $CKAN__PLUGINS == *"xloader"* ]]; then
+   # Xloader settings have been configured in the .env file
+   # Set API token if necessary
+   if [ -z "$CKANEXT__XLOADER__API_TOKEN" ] ; then
+      echo "Set up ckanext.xloader.api_token in the CKAN config file"
+      ckan config-tool $CKAN_INI "ckanext.xloader.api_token=$(ckan -c $CKAN_INI user token add ckan_admin xloader | tail -n 1 | tr -d '\t')"
+      ckan config-tool $CKAN_INI "ckanext.xloader.jobs_db.uri = $CKAN_SQLALCHEMY_URL"
+   fi
+else
+   echo "Not configuring Xloader"
+fi
